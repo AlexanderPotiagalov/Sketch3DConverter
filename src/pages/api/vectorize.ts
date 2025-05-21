5b13e098d0a2a4588f83f9e45d172e901400473a
AlexanderPotiagalov <apa168@sfu.ca>
2025-05-21 14:39:51 -0700

    Set up the scaffold of the API routes of pages/api/vectorize.ts to convert user-drawn strokes and recognized shapes into 3D extrusion data

---
+++ src/pages/api/vectorize.ts
@@ -0,0 +1,9 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  res.setHeader("Allow", "POST");
+  return res.status(405).end("Only POST allowed");
+}
